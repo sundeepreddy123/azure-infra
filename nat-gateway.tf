7674bdcac2a57cdfd650efa5508bf55a7ef0cf79
sundeep reddy raju <r.sundeepreddy786@gmail.com>
2026-02-17 00:04:32 +0530

    Update nat-gateway.tf

--- nat-gateway.tf
+++ nat-gateway.tf
@@ -1,1 +1,13 @@
+resource "azurerm_public_ip" "nat_pip" {
+  name                = "nat-pip"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
 
+resource "azurerm_nat_gateway" "main" {
+  name                = "nat-gateway"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
+}
