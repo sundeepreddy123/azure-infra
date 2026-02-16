c4a863409726b1e059ce3184b6b1b4f5e934dada
sundeep reddy raju <r.sundeepreddy786@gmail.com>
2026-02-17 00:06:53 +0530

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -1,1 +1,12 @@
+resource "azurerm_network_interface" "nic" {
+  name                = "vm-nic"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.private.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
 
