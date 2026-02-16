9a6a8829c27779d48f546f0bb730300ca6c4cd50
sundeep reddy raju <r.sundeepreddy786@gmail.com>
2026-02-17 00:02:43 +0530

    Update nsg.tf

--- nsg.tf
+++ nsg.tf
@@ -1,1 +1,18 @@
+resource "azurerm_network_security_group" "vm_nsg" {
+  name                = "vm-nsg"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  security_rule {
+    name                       = "allow-ssh-vnet"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_address_prefix      = "VirtualNetwork"
+    destination_port_range     = "22"
+    source_port_range          = "*"
+    destination_address_prefix = "*"
+  }
+}
 
