f2628e017de40f5fa619982fee88ef83dccca49f
sundeep reddy raju <r.sundeepreddy786@gmail.com>
2026-02-17 00:04:49 +0530

    Update route-table.tf

--- route-table.tf
+++ route-table.tf
@@ -1,1 +1,6 @@
+resource "azurerm_route_table" "private_rt" {
+  name                = "private-rt"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
+}
 
