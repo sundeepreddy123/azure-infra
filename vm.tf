8905df8617756e69efef6f11722d1098faf16ad8
sundeep reddy raju <r.sundeepreddy786@gmail.com>
2026-02-17 00:14:28 +0530

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -10,3 +10,34 @@ resource "azurerm_network_interface" "nic" {
   }
 }
 
+##################################################
+# VM
+##################################################
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "test-vm"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  size                = "Standard_B2s"
+  admin_username      = "azureuser"
+
+  network_interface_ids = [
+    azurerm_network_interface.vm_nic.id
+  ]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
