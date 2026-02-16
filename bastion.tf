80b21807ac7483147dc4b5b7d1e711e8c4553bb0
sundeep reddy raju <r.sundeepreddy786@gmail.com>
2026-02-17 00:05:02 +0530

    Update bastion.tf

--- bastion.tf
+++ bastion.tf
@@ -1,1 +1,20 @@
+resource "azurerm_public_ip" "bastion_pip" {
+  name                = "bastion-pip"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_bastion_host" "main" {
+  name                = "main-bastion"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  ip_configuration {
+    name                 = "config"
+    subnet_id            = azurerm_subnet.bastion.id
+    public_ip_address_id = azurerm_public_ip.bastion_pip.id
+  }
+}
 
