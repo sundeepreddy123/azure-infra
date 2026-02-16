a384e349237fd33bb3f64f2c3207484f26f16faf
sundeep reddy raju <r.sundeepreddy786@gmail.com>
2026-02-16 23:57:21 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,5 +8,17 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}
+  tenant_id    =  var.tenant_id
+  subscription_id  =  var.sub
+  client_id        =  var.client
+  clientsecret     =  var.client_secret
+  patner_id        =  "XXXXXXXXXXXXXXXXXXX"
+  feature {
+    key_vault {
+      purge_soft_delete_on_destroy   =  true
+    }
+    recovery_service_vaults  {
+      recover_soft_deleted_backup_protected_vm  =  true
+      }
+    }
 }
